f9bbc1bc90be17abf4b704fb2ede5edfd4126c5a
tuhinhossain123 <csetuhin55@gmail.com>
2025-07-08 13:17:42 +0600

    redux todo app project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h2 className="">heloo dev</h2>
+      <Button>click here</Button>
+    </div>
+  );
+}
+
+export default App;
